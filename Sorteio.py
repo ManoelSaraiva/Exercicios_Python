f53b7515edbf820551fbceef60af2a7af604692d
ManoelSaraiva <manoeljsaraiva@gmail.com>
2022-08-23 00:37:50 -0300

    Incrementação Classe Sorteio - sorteio_aleatorio

--- Sorteio.py
+++ Sorteio.py
@@ -2,21 +2,27 @@
     Classe para realização de sorteio de jogos Lotofácil
 
 '''
+import random
 
 
 class Sorteio():
 
-    #self.dezenas_lotofacil = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
-
     def __init__(self):
-        self.dezenas_lotofacil = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
-        
+        pass
 
     def sorteio_aleatorio(self):
-        for dezena in self.dezenas_lotofacil:
-            print(dezena)
+        self.__dezenas_lotofacil = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                                    11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+        self.sorteio = []
+        for x in range(15):
+            self.sorteio.append(self.__dezenas_lotofacil.pop(
+                random.randrange(0, len(self.__dezenas_lotofacil))))
+        self.sorteio.sort()
+        return self.sorteio
 
 
 dezenas2 = Sorteio()
 
-dezenas2.sorteio_aleatorio()
+print(dezenas2.sorteio_aleatorio())
+
+
