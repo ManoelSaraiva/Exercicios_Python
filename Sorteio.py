f094b513e47982a14b59fd27f5e8732d90ee4b8a
ManoelSaraiva <manoeljsaraiva@gmail.com>
2022-08-23 19:16:16 -0300

    Classe Sorteio.py terminado gerador de jogos.

--- Sorteio.py
+++ Sorteio.py
@@ -1,33 +1,31 @@
-'''
+"""
     Classe para realização de sorteio de jogos Lotofácil
-
-'''
+"""
 import random
 
 
-class Sorteio():
+class Sorteio:
 
     def __init__(self):
+        self.__jogos = None
         self.__dezenas_lotofacil = None
 
     def gerar_jogos(self, qtd=1):
-        self.__dezenas_lotofacil = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
-                                  12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
-        self.jogos = []
+        self.__jogos = []
         for x in range(qtd):
-            self.jogos.append(self.__sorteio_aleatorio(self.__dezenas_lotofacil))
-        return self.jogos
+            self.__dezenas_lotofacil = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+                                        12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+            self.__jogos.insert(x, self.__sorteio_aleatorio(self.__dezenas_lotofacil))
+        return self.__jogos
 
     def __sorteio_aleatorio(self, dezenas):
-        self.sorteio = []
+        self.__sorteio = []
         for x in range(15):
-            self.sorteio.append(dezenas.pop(random.randrange(0, len(dezenas))))
-        self.sorteio.sort()
-        return self.sorteio
+            self.__sorteio.append(dezenas.pop(random.randrange(0, len(dezenas))))
+        self.__sorteio.sort()
+        return self.__sorteio
 
 
-dezenas2 = Sorteio()
-
-
-# print(dezenas2.sorteio_aleatorio(dezenas_lot))
-print(dezenas2.gerar_jogos(1))
+if __name__ == "__main__":
+    dezenas2 = Sorteio()
+    print(dezenas2.gerar_jogos(5))
