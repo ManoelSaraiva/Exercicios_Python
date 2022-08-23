c6989e81cffd18d4b91f2f0a09e5ca0449e55752
ManoelSaraiva <manoeljsaraiva@gmail.com>
2022-08-23 00:42:21 -0300

    Alteração Classe Sorteio

--- Sorteio.py
+++ Sorteio.py
@@ -6,13 +6,13 @@ import random
 
 
 class Sorteio():
+    __dezenas_lotofacil = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                           11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
 
     def __init__(self):
         pass
 
     def sorteio_aleatorio(self):
-        self.__dezenas_lotofacil = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-                                    11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
         self.sorteio = []
         for x in range(15):
             self.sorteio.append(self.__dezenas_lotofacil.pop(
@@ -24,5 +24,3 @@ class Sorteio():
 dezenas2 = Sorteio()
 
 print(dezenas2.sorteio_aleatorio())
-
-
