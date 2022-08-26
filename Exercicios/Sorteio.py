61b4e9cb3225272448e772d3f2dfed17b2cd6613
ManoelSaraiva <manoeljsaraiva@gmail.com>
2022-08-26 20:47:32 -0300

    Adiçao do poetry e limpeza

--- Exercicios/Sorteio.py
+++ Exercicios/Sorteio.py
@@ -1,11 +1,10 @@
 """
-    Classe para realização de sorteio de jogos Lotofácil
+Classe para realização de sorteio de jogos Lotofácil
 """
 import random
 
 
 class Sorteio:
-
     def __init__(self):
         self.__jogos = None
         self.DENZENAS_LOTOFACIL = None
@@ -13,21 +12,48 @@ class Sorteio:
     def gerar_jogos(self, qtd=1):
         self.__jogos = []
         for x in range(qtd):
-            self.__DENZENAS_LOTOFACIL = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 
-                    12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
-            self.__jogos.insert(x, self.__sorteio_aleatorio(
-                self.__DENZENAS_LOTOFACIL))
+            self.__DENZENAS_LOTOFACIL = [
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9,
+                10,
+                11,
+                12,
+                13,
+                14,
+                15,
+                16,
+                17,
+                18,
+                19,
+                20,
+                21,
+                22,
+                23,
+                24,
+                25,
+            ]
+            self.__jogos.insert(
+                x, self.__sorteio_aleatorio(self.__DENZENAS_LOTOFACIL)
+            )
         return self.__jogos
 
     def __sorteio_aleatorio(self, dezenas):
         self.__sorteio = []
-        for x in range(15):
-            self.__sorteio.append(dezenas.pop(
-                random.randrange(0, len(dezenas))))
+        for _ in range(15):
+            self.__sorteio.append(
+                dezenas.pop(random.randrange(0, len(dezenas)))
+            )
         self.__sorteio.sort()
         return self.__sorteio
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     dezenas2 = Sorteio()
     print(dezenas2.gerar_jogos(5))
